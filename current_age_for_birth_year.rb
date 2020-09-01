463701d08459c87cb511ddaf761e15d9b95b0b24
Devin Cooper <dcoope12@asu.edu>
2020-09-01 04:15:27 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,4 +1,11 @@
+<<<<<<< HEAD
 
 def current_age_for_birth_year(birth_year)
   2003 - birth_year
 end
+=======
+```ruby
+def current_age_for_birth_year
+end
+```
+>>>>>>> 6963363e1194c4c2366bb2c0a0c5b7c79b714fc7
